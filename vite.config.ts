ff47b526f2537a287eb655d1ad978327c373cb47
fayzaya <fayzayaganangputra20@gmail.com>
2025-10-17 03:30:18 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sewabusmurahjogja/', // <- penting, nama repo kamu
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
